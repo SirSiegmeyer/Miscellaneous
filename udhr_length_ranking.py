3d55e71a54fd588d1a8b428e7e57e86768c231ad
Simon Engstler <SimonEngstler@live.de>
2022-11-12 19:54:26 +0100

    improvements

--- udhr_length_ranking.py
+++ udhr_length_ranking.py
@@ -6,7 +6,6 @@ import PyPDF2, io, requests
 import re
 
 
-
 if __name__ == "__main__":
     # Create an URL object
     url = 'http://research.ics.aalto.fi/cog/data/udhr/'
@@ -16,30 +15,23 @@ if __name__ == "__main__":
     # parser-lxml = Change html to Python friendly format
     # Obtain page's information
     soup = BeautifulSoup(page.text, 'lxml')
-    soup
-
-    #print(soup)
 
     hrefs = []
     names = []
 
     for a in soup.find_all('a', href=True, text="pdf"):
         hrefs.append(a['href'])
+        names.append(soup.find('a', href=re.compile("LangID=" + a['href'][4:7])).text)
 
-    for href in hrefs:
-        names.append(soup.find('a', href=re.compile("LangID=" + href[4:7])).text)
-
-    fullURL = "http://research.ics.aalto.fi/cog/data/udhr/"
+    url = "http://research.ics.aalto.fi/cog/data/udhr/"
 
     myDict = {}
-    i = 0
 
-    for a in hrefs:
-        response = requests.get(fullURL + a)
+    for name, a in zip(names, hrefs):
+        response = requests.get(url + a)
         pdf_file = io.BytesIO(response.content)  # response being a requests Response object
         pdf_reader = PyPDF2.PdfFileReader(pdf_file)
         num_pages = pdf_reader.numPages
-        myDict[names[i]] = num_pages
-        i += 1
+        myDict[name] = num_pages
 
     print(sorted(myDict.items(), key=lambda item: item[1]))
